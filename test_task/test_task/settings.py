f22bbfd3258f3bda540b3370bacdca123814fbd9
Денис Марьянский <d.maryanskiy@gmail.com>
2021-09-05 18:19:40 +0300

    release v.1.0

--- test_task/test_task/settings.py
+++ test_task/test_task/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v*$ebh0p!b8g9op!36zg+b!vs8dz@la%nhw9o3k*z$pyge#x*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
